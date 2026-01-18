3da62440d39bb58e9f4eb517cd9088c5ac196d1b
kishan885 <chauhankishan275@gmail.com>
2026-01-18 11:41:04 +0530

    fix: minor change

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,39 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use  \Illuminate\Support\Facades\Artisan;
+
+Route::get('/all-migrate/{path}', function ($path) {
+    Artisan::call('migrate', [
+        '--path' => $path,
+        '--force' => true
+    ]);
+});
+
+Route::get('/all-rollback', function () {
+    Artisan::call('migrate:rollback', ['--force' => true]);
+});
+
+Route::get('/migrate-fresh', function () {
+    Artisan::call('migrate:fresh', ['--force' => true]);
+});
+
+Route::get('/db-seed', function () {
+    Artisan::call('db:seed', ['--force' => true]);
+});
+
+Route::get('/db-seed/{class}', function ($class) {
+    Artisan::call('db:seed', [
+        '--class' => $class,
+        '--force' => true
+    ]);
+});
+
+Route::get('/optimize:clear', function () {
+    Artisan::call('optimize:clear');
+});
+
+
 
 Route::get('/', function () {
     return view('app');
@@ -8,3 +41,6 @@ Route::get('/', function () {
 
 Route::view('/chat/{any?}', 'app')->where('any', '.*');
 Route::view('/{any}', 'app')->where('any', '.*');
+Route::get('/all-migrate', function () {
+    \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+});
